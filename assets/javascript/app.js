4bae2539c73692d8b75b65a91d9cb3c62940cbf1
dcal588 <calderon.david@gmail.com>
2017-07-31 17:03:58 -0700

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,16 @@
-
-    $("button").on("click", function() {
-      var person = $(this).attr("data-person");
-      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        person + "&api_key=25e6b8b7abc14f53a86853319e29c240&limit=10";
-
-      $.ajax({
+$ (document).ready( function(){	
+var topics = ["The Matrix", "Training Day", "Forrest Gump", "Star Trek", "Star Wars"];
+      
+    function displayer() {
+        var topics = $(this).attr("data-name");
+        var queryURL = var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+        topics + "&api_key=25e6b8b7abc14f53a86853319e29c240&limit=10";
+        
+        $.ajax({
           url: queryURL,
           method: "GET"
-        })
-        .done(function(response) {
-          var results = response.data;
-
-          for (var i = 0; i < results.length; i++) {
-            var gifDiv = $("<div class='item'>");
-
-            var rating = results[i].rating;
-
-            var p = $("<p>").text("Rating: " + rating);
-
-            var personImage = $("<img>");
-            personImage.attr("src", results[i].images.fixed_height.url);
-
-            gifDiv.prepend(p);
-            gifDiv.prepend(personImage);
-
-            $("#gifs-appear-here").prepend(gifDiv);
-          }
-        });
-    });
+        }).done(function(response) {
+          var topicsBox = $("<div class='topics'>");
+          var ratingBox = response.rating;
+          var paragraphRating = $("<p>").text("Rating: " + ratingBox);
+          topicsBox.append(paragraphRating);
