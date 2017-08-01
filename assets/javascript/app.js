0c3c3ae92d4b886376a5c8170a6fc14d10681808
David Calderon <David@DESKTOP-7UAHA94.(none)>
2017-07-31 22:24:35 -0700

    Initial Commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,26 +1,37 @@
 $ (document).ready( function(){	
-var topics = ["The Matrix", "Training Day", "Forrest Gump", "Star Trek", "Star Wars"];
+	var topics = ["The Matrix", "Training Day", "Forrest Gump", "Star Trek", "Star Wars"];
 
-
-    $("#topic-button").on("click", function() {
-      // Storing our giphy API URL for a random cat image
-      var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cats";
-      // Perfoming an AJAX GET request to our queryURL
-      $.ajax({
-        url: queryURL,
-        method: "GET"
-      })
-      // After the data from the AJAX request comes back
-      .done(function(response) {
-        // Saving the image_original_url property
-        var imageUrl = response.data.image_original_url;
-        // Creating and storing an image tag
-        var topicImage = $("<img>");
-        // Setting the catImage src attribute to imageUrl
-        topicImage.attr("src", imageUrl);
-        topicImage.attr("alt", "cat image");
-        // Prepending the catImage to the images div
-        $("#images").prepend(topicImage);
-});
-    });
+  $("#topic-buttons").on("click", function() {
+     var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cats";
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+    .done(function(response) {
+      var imageUrl = response.data.image_original_url;
+      var topicImage = $("<img>");
+      topicImage.attr("src", imageUrl);
+      topicImage.attr("alt", "topic image");
+      $("#images").prepend(topicImage);
+		});
   });
+
+  function createButtons() {
+        $("#topicBox").empty();
+        for (var i = 0; i < topics.length; i++) {
+          var a = $("<button>");
+          a.addClass("topic");
+          a.attr("data-name", topics[i]);
+          a.text(topics[i]);
+          $("#topicBox").append(a);
+        }
+  }
+  $("#add-topic").on("click", function(event) {
+        event.preventDefault();
+        var topic = $("#topic-input").val();
+        topics.push(topic);
+        // Calling renderButtons which handles the processing of our movie array
+        createButtons();
+      });
+  createButtons();
+});
